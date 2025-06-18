b18d729e8ff5965b6ffc39bcc9ec2d8275a910a1
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-06-18 20:07:34 +0700

    feat: create notification model

---
+++ src/modules/notification/notification.model.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export enum NotificationType {
+  ORDER = 'order',
+  SYSTEM = 'system',
+  VOUCHER = 'voucher',
+  ADMIN = 'admin',
+}
+
+export interface INotification extends Document {
+  userId: mongoose.Types.ObjectId;
+  title: string;
+  content: string;
+  metadata?: Record<string, any>;
+  type: NotificationType;
+  isRead: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const notificationSchema = new Schema<INotification>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true, trim: true },
+    content: { type: String, required: true, trim: true },
+    metadata: { type: Object },
+    type: {
+      type: String,
+      enum: Object.values(NotificationType),
+      required: true,
+    },
+    isRead: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model<INotification>(
+  'Notification',
+  notificationSchema,
+  'notifications'
+);
+
+export default Notification;
